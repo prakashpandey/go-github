e1b07156a83d8ac4326db126238ce6a7b87b824d
Prakash Pandey <prakashpandeyy@yahoo.com>
2019-08-14 03:40:08 +0530

    added example for api search users by loc and lang

--- example/main.go
+++ example/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/prakashpandey/go-github/search"
+)
+
+func searchUser() []search.UserSearch {
+	return search.Users(search.NewFilter("golang", "delhi", 100, 0))
+}
+
+func main() {
+	totalUsers := searchUser()
+	fmt.Printf("Total users: %d\n", len(totalUsers))
 }
