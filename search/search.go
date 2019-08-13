e16a88e174e458d0cfc6d9dcb6dc4035e367c282
Prakash Pandey <prakashpandeyy@yahoo.com>
2019-08-14 03:39:24 +0530

    fixed search user by lang and loc api

--- search/search.go
+++ search/search.go
@@ -18,15 +18,15 @@ type UserSearchResponse struct {
 
 // UserSearch is used to map the response of user search API.
 type UserSearch struct {
-	Login     string `json:"login"`
-	ID        string `json:"id"`
-	NodeID    string `json:"node_id"`
-	AvatarURL string `json:"avatar_url"`
-	URL       string `json:"url"`
-	HTMLURL   string `json:"html_url"`
-	Type      string `json:"type"`
-	SiteAdmin string `json:"site_admin"`
-	Score     int    `json:"score"`
+	Login     string  `json:"login"`
+	ID        int     `json:"id"`
+	NodeID    string  `json:"node_id"`
+	AvatarURL string  `json:"avatar_url"`
+	URL       string  `json:"url"`
+	HTMLURL   string  `json:"html_url"`
+	Type      string  `json:"type"`
+	SiteAdmin bool    `json:"site_admin"`
+	Score     float64 `json:"score"`
 }
 
 const defaultPerPageUserSearchLimit = 100
@@ -42,15 +42,27 @@ type UserFilter struct {
 	Page int
 }
 
+// NewFilter returns new filter object.
+func NewFilter(lang, loc string, perPage, page int) *UserFilter {
+	f := &UserFilter{Lang: lang, Location: loc, Page: page}
+	if perPage == 0 {
+		f.PerPage = defaultPerPageUserSearchLimit
+	}
+	return f
+}
+
 // Users searches github users based on the filter criteria.
 func Users(filter *UserFilter) []UserSearch {
-	url := fmt.Sprintf("%s%s", conf.APIURL, "/search/users?")
+	url := fmt.Sprintf("%s%s", conf.APIURL, "/search/users?q=")
 	addQualifier := func(qualifier string) {
-		if strings.HasSuffix(url, "?") {
+		if strings.HasSuffix(url, "q=") {
 			url = fmt.Sprintf("%s%s", url, qualifier)
 		} else {
 			url = fmt.Sprintf("%s+%s", url, qualifier)
 		}
+	}
+	addParam := func(param string) {
+		url = fmt.Sprintf("%s&%s", url, param)
 	}
 	if filter.Lang != "" {
 		addQualifier(fmt.Sprintf("language:%s", filter.Lang))
@@ -58,41 +70,51 @@ func Users(filter *UserFilter) []UserSearch {
 	if filter.Location != "" {
 		addQualifier(fmt.Sprintf("location:%s", fmt.Sprintf("%s", filter.Location)))
 	}
-	if filter.PerPage != 0 {
+	if filter.PerPage > 0 {
 		addQualifier(fmt.Sprintf("&per_page=%s", fmt.Sprintf("%d", filter.PerPage)))
 	} else {
-		addQualifier(fmt.Sprintf("&per_page=%s", fmt.Sprintf("%d", defaultPerPageUserSearchLimit)))
+		filter.PerPage = defaultPerPageUserSearchLimit
+		addParam(fmt.Sprintf("per_page=%s", fmt.Sprintf("%d", filter.PerPage)))
 	}
-	getUsers := func(page int) (*http.Response, error) {
-		if page > 0 {
-			addQualifier(fmt.Sprintf("&page=%s", fmt.Sprintf("%d", filter.Page)))
-		}
-		return http.Get(url)
-	}
-	// upperBound is used to run loop at least onces.
-	var upperBound int
-	if filter.Page < 1 {
-		upperBound = 1
-	} else {
-		upperBound = filter.Page
-	}
-	var users []UserSearch
-	for i := 1; i <= upperBound; i++ {
-		if filter.Page < 1 {
-			upperBound = filter.Page
-		}
-		var resp *http.Response
-		resp, err := getUsers(upperBound)
+	getUsers := func(page int) ([]UserSearch, int) {
+		addParam(fmt.Sprintf("page=%s", fmt.Sprintf("%d", filter.Page)))
+		var users []UserSearch
+		fmt.Printf("url: %s\n", url)
+		resp, err := http.Get(url)
 		if err != nil {
-			break
+			return users, 0
 		}
 		searchResp := &UserSearchResponse{}
 		if err := json.NewDecoder(resp.Body).Decode(searchResp); err != nil {
-			for _, usr := range searchResp.Items {
+			fmt.Printf("error decoding response: %s", err)
+			return users, 0
+		}
+		for _, usr := range searchResp.Items {
+			users = append(users, usr)
+		}
+		_ = resp.Body.Close()
+		return users, searchResp.TotalCount
+	}
+	var users []UserSearch
+	if filter.Page > 1 {
+		users, _ = getUsers(filter.Page)
+	} else {
+		// Find all users.
+		page := 1 // initial page
+		for i := 1; i <= page; i++ {
+			usrs, total := getUsers(page)
+			// In the first iteration, calculate the page size.
+			if i == 1 {
+				if total%filter.PerPage == 0 {
+					page = total / filter.PerPage
+				} else {
+					page = total/filter.PerPage + 1
+				}
+			}
+			for _, usr := range usrs {
 				users = append(users, usr)
 			}
 		}
-		_ = resp.Body.Close()
 	}
 	return users
 }
