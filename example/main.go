e93d54b1c109f4eb45c9c295b59cff2a77b11072
Prakash Pandey <prakashpandeyy@yahoo.com>
2019-08-14 04:12:15 +0530

    implemented get github's user profile with example

--- example/main.go
+++ example/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"github.com/prakashpandey/go-github/search/user"
+
 	"github.com/prakashpandey/go-github/search"
 )
 
@@ -10,7 +12,14 @@ func searchUser() []search.UserSearch {
 	return search.Users(search.NewFilter("golang", "delhi", 100, 0))
 }
 
+func getUserProfile(username string) *user.User {
+	return user.GetUser(username)
+}
 func main() {
-	totalUsers := searchUser()
-	fmt.Printf("Total users: %d\n", len(totalUsers))
+	users := searchUser()
+	fmt.Printf("Total users: %d\n", len(users))
+	for _, user := range users {
+		userProfile := getUserProfile(user.Login)
+		fmt.Printf("username: %s name: %s email: %s location: %s hireable: %t ", userProfile.Login, userProfile.Name, userProfile.Email, userProfile.Location, userProfile.Hireable)
+	}
 }
--- search/user/user.go
+++ search/user/user.go
@@ -1,5 +1,44 @@
 package user
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/prakashpandey/go-github/conf"
+)
+
 // User is a github user's data structure.
 type User struct {
+	Login     string  `json:"login"`
+	ID        int     `json:"id"`
+	NodeID    string  `json:"node_id"`
+	AvatarURL string  `json:"avatar_url"`
+	URL       string  `json:"url"`
+	HTMLURL   string  `json:"html_url"`
+	Type      string  `json:"type"`
+	SiteAdmin bool    `json:"site_admin"`
+	Score     float64 `json:"score"`
+	Name      string  `json:"name"`
+	Company   string  `json:"company"`
+	Email     string  `json:"email"`
+	Location  string  `json:"location"`
+	Hireable  bool    `json:"hireable"`
+}
+
+// GetUser returns user profile data.
+func GetUser(username string) *User {
+	url := fmt.Sprintf("%s%s%s", conf.APIURL, "/users", fmt.Sprintf("/%s", username))
+	fmt.Printf("user profile url: %s\n", url)
+	var user = &User{}
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("\nerror finding user: %s", err)
+		return nil
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(user); err != nil {
+		fmt.Printf("\nerror decoding response: %s", err)
+	}
+	return user
 }
